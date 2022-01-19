67b0f34f735c8ec8dd887ff7d3f7fa8d78b6085c
Francesco <francesco@monzo.com>
2022-01-19 20:29:52 +0100

    added conditionals to program

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,17 @@
 def fizzbuzz(number)
-  if number % 3 == 0
-  puts "fizz"
-  return "fizz"
+  if number % 3 == 0 && number % 5 == 0
+    puts "fizzbuzz"
+    return "fizzbuzz"
+  elsif number % 3 == 0
+    puts "fizz"
+    return "fizz"
+  elsif number & 5 == 0
+    puts "buzz"
+    return "buzz"
+  else
+    puts number
+    return number
   end
 end
 
-fizzbuzz(3)
+fizzbuzz(345)
